cb333d8e81fa16f7f4f402f77a9bfa27a056f1b6
katerinazk <katziaka2001@gmail.com>
2026-04-04 19:39:06 +0300

    αφαίρεση κάποιων σχολίων

--- supabase.ts
+++ supabase.ts
@@ -2,7 +2,6 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-// ΒΑΛΕ ΤΑ ΔΙΚΑ ΣΟΥ ΚΛΕΙΔΙΑ ΕΔΩ ΜΕΣΑ ΣΤΑ ΑΥΤΑΚΙΑ
 const supabaseUrl = 'https://xlmpzemrubhmevcnyluv.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsbXB6ZW1ydWJobWV2Y255bHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyODczNDAsImV4cCI6MjA5MDg2MzM0MH0.4-_R0yZ7u_uUdcp83O8tWqdR3rsoIMkuWaIFTRu67Q0';
 
